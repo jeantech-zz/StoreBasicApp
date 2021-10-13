eab148df564608cccf94ad7dade1575b75c7715b
Jennifer <jeante05@gmail.com>
2021-10-13 14:16:38 -0500

    configuracion del proyecto

---
+++ database/migrations/2021_10_13_173623_create_requests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->nullable()->constrained('orders');
+            $table->string('url_app', 100);
+            $table->string('url_payment', 100);
+            $table->string('response', 250);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
